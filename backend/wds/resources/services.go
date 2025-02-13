f5752ed009f14574808512b5e1f43a782c92e506
Rahul Vishwakarma <rahulvs2809@gmail.com>
2025-02-13 19:03:38 +0530

    added helperServiceDetails

--- backend/wds/resources/services.go
+++ backend/wds/resources/services.go
@@ -1,7 +1,9 @@
 package resources
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/katamyra/kubestellarUI/wds"
@@ -9,6 +11,80 @@ import (
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
 )
+
+type Label struct {
+	Component string `json:"component"`
+	Provider  string `json:"provider"`
+}
+type MetaDataDetail struct {
+	Name              string    `json:"name"`
+	Namespace         string    `json:"namespace"`
+	Uid               string    `json:"uid"`
+	ResourceVersion   string    `json:"resourceVersion"`
+	CreationTimestamp time.Time `json:"creationTimestamp"`
+	Labels            Label     `json:"labels,omitempty"`
+	ManagedFields     []struct {
+		Manager    string    `json:"manager"`
+		Operation  string    `json:"operation"`
+		ApiVersion string    `json:"apiVersion"`
+		Time       time.Time `json:"time"`
+		FieldsType string    `json:"fieldsType"`
+	} `json:"managedFields"`
+}
+
+type Status struct {
+	LoadBalancer corev1.LoadBalancerStatus `json:"loadBalancer,omitempty"`
+}
+type ServiceDetail struct {
+	Metadata MetaDataDetail `json:"metadata"`
+	Status   Status         `json:"status"`
+}
+
+func helperServiceDetails(service corev1.Service) ServiceDetail {
+	serviceDetail := ServiceDetail{
+		Metadata: MetaDataDetail{
+			Name:              service.Name,
+			Namespace:         service.Namespace,
+			Uid:               string(service.UID),
+			ResourceVersion:   service.ResourceVersion,
+			CreationTimestamp: service.CreationTimestamp.Time,
+			ManagedFields: make([]struct {
+				Manager    string    `json:"manager"`
+				Operation  string    `json:"operation"`
+				ApiVersion string    `json:"apiVersion"`
+				Time       time.Time `json:"time"`
+				FieldsType string    `json:"fieldsType"`
+			}, len(service.ManagedFields)),
+		},
+		Status: struct {
+			LoadBalancer corev1.LoadBalancerStatus `json:"loadBalancer,omitempty"`
+		}{
+			LoadBalancer: service.Status.LoadBalancer,
+		},
+	}
+	if service.Labels != nil {
+		serviceDetail.Metadata.Labels.Component = service.Labels["component"]
+		serviceDetail.Metadata.Labels.Provider = service.Labels["provider"]
+	}
+
+	// Copy managed fields
+	for i, field := range service.ManagedFields {
+		serviceDetail.Metadata.ManagedFields[i] = struct {
+			Manager    string    `json:"manager"`
+			Operation  string    `json:"operation"`
+			ApiVersion string    `json:"apiVersion"`
+			Time       time.Time `json:"time"`
+			FieldsType string    `json:"fieldsType"`
+		}{
+			Manager:    field.Manager,
+			Operation:  string(field.Operation),
+			ApiVersion: field.APIVersion,
+			Time:       field.Time.Time,
+			FieldsType: field.FieldsType,
+		}
+	}
+	return serviceDetail
+}
 
 func GetServiceList(ctx *gin.Context) {
 	namespace := ctx.Param("namespace")
@@ -31,10 +107,15 @@ func GetServiceList(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusAccepted, gin.H{
-		"services": services,
-	})
+	var servicesList []ServiceDetail
+	for _, service := range services.Items {
+		serviceDetail := helperServiceDetails(service)
+		servicesList = append(servicesList, serviceDetail)
+	}
 
+	ctx.JSON(http.StatusOK, gin.H{
+		"services": servicesList,
+	})
 }
 
 func GetServiceByServiceName(ctx *gin.Context) {
@@ -53,14 +134,10 @@ func GetServiceByServiceName(ctx *gin.Context) {
 		ctx.JSON(http.StatusNotFound, gin.H{"error": "Services not found", "err": err.Error()})
 		return
 	}
+	serviceDetail := helperServiceDetails(*services)
 
 	ctx.JSON(http.StatusAccepted, gin.H{
-		"name":              services.Name,
-		"namespace":         services.Namespace,
-		"uid":               services.UID,
-		"creationTimestamp": services.CreationTimestamp.Time,
-		"services":          services,
-		"status":            services.Status,
+		"service": serviceDetail,
 	})
 
 }
