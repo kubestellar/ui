572be28b0a990dfa69d8444794f8123053736731
alikhere <alikhurshidhere@gmail.com>
2025-02-16 02:24:30 +0530

    fix: alignment of Kebeconfig onboard in Cluster category for frontend (#170)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,7 +18,7 @@ export function Layout() {
       <div>
         <Header />
         <div className="w-full flex gap-0 pt-20 xl:pt-[96px] 2xl:pt-[112px] mb-auto">
-          <div className="hidden xl:block xl:w-[250px] 2xl:w-[280px] 3xl:w-[350px] border-r-2 border-base-300 dark:border-slate-700 px-3 xl:px-4 xl:py-1">
+        <div className="hidden xl:block xl:w-[285px] 2xl:w-[310px] 3xl:w-[350px] border-r-2 border-base-300 dark:border-slate-700 px-3 xl:px-4 xl:py-1">
             <Menu />
           </div>
           <div className="w-full px-4 xl:px-4 2xl:px-5 xl:py-2 overflow-clip">
