5fab0f6bd17af82d9c310abe8439a20d9cc2ee61
Rahul Vishwakarma <rahulvs2809@gmail.com>
2025-02-18 11:51:44 +0530

    feat: custom k8s controller using informer

--- backend/main.go
+++ backend/main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"github.com/katamyra/kubestellarUI/wds"
 	"k8s.io/client-go/informers"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/util/homedir"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
 	"time"
 
@@ -83,23 +79,7 @@ func main() {
 	})
 
 	router.GET("/logs/hello", func(ctx *gin.Context) {
-		var kubeconfig *string
-		if home := homedir.HomeDir(); home != "" {
-			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-		} else {
-			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-		}
-		flag.Parse()
-
-		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-		if err != nil {
-			fmt.Println("%s", err.Error())
-			config, err = rest.InClusterConfig()
-			if err != nil {
-				panic(err)
-			}
-		}
-		clientset, err := kubernetes.NewForConfig(config)
+		clientset, err := wds.GetClientSetKubeConfig()
 		if err != nil {
 			panic(err)
 		}
--- backend/wds/controller.go
+++ backend/wds/controller.go
@@ -5,12 +5,15 @@ package wds
 
 import (
 	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/util/wait"
 	appsinformers "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
 	appslisters "k8s.io/client-go/listers/apps/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
 	"time"
 )
 
@@ -24,32 +27,32 @@ type Controller struct {
 
 func NewController(clientset kubernetes.Interface,
 	deploymentInformer appsinformers.DeploymentInformer) *Controller {
-	// DOCS: https://github.com/kubernetes/sample-controller/blob/8ab9f14766821df256ea5234629493d2b66ab89d/controller.go#L110-L114
 
-	//ratelimiter := workqueue.NewTypedMaxOfRateLimiter(
-	//	workqueue.NewTypedItemExponentialFailureRateLimiter[cache.ObjectName](5*time.Minute, 1000*time.Second),
-	//	&workqueue.TypedBucketRateLimiter[cache.ObjectName]{Limiter: rate.NewLimiter(rate.Limit(50), 300)})
-
+	/*
+		 DOCS: https://github.com/kubernetes/sample-controller/blob/8ab9f14766821df256ea5234629493d2b66ab89d/controller.go#L110-L114
+		ratelimiter := workqueue.NewTypedMaxOfRateLimiter(
+			workqueue.NewTypedItemExponentialFailureRateLimiter[cache.ObjectName](5*time.Minute, 1000*time.Second),
+			&workqueue.TypedBucketRateLimiter[cache.ObjectName]{Limiter: rate.NewLimiter(rate.Limit(50), 300)})
+	*/
 	controller := &Controller{
 		clientset:         clientset,
 		deploymentsLister: deploymentInformer.Lister(),
 		deploymentsSynced: deploymentInformer.Informer().HasSynced,
-		//workqueue:         workqueue.NewTypedRateLimitingQueue(ratelimiter),
-		workqueue: workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "kubestellar"),
+		workqueue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "deploymentQueue"),
 	}
 
 	// Set up an event handler for when Deployment resources change
 	deploymentInformer.Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
-			AddFunc: controller.handleAdd,
-			//UpdateFunc: controller.handleUpdate, // have some other format
+			AddFunc:    controller.handleAdd,
+			UpdateFunc: controller.handleUpdate,
 			DeleteFunc: controller.handleDel,
 		})
 	return controller
 }
 
 func (c *Controller) Run(ch <-chan struct{}) {
-	fmt.Sprintf("starting controller")
+	fmt.Printf("starting controller")
 	if !cache.WaitForCacheSync(ch, c.deploymentsSynced) {
 		fmt.Printf("failed to wait for caches to sync")
 	}
@@ -63,22 +66,32 @@ func (c *Controller) worker() {
 }
 
 func (c *Controller) processItem() bool {
-	item, shutdown := c.workqueue.Get()
+	objRef, shutdown := c.workqueue.Get()
 	if shutdown {
 		return false
 	}
 	// we do not process the item again
-	defer c.workqueue.Forget(item)
-	_, err := cache.MetaNamespaceKeyFunc(item)
+	defer c.workqueue.Done(objRef)
+	key, err := cache.MetaNamespaceKeyFunc(objRef)
 	if err != nil {
 		fmt.Printf("key and err, %s\n", err.Error())
 	}
-	//namespace, name, err := cache.SplitMetaNamespaceKey(key)
-	//if err != nil {
-	//	fmt.Printf("spliting namespace and name, %s\n", err.Error())
-	//}
-	//err = c.syncDeployment(namespace, name)
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		fmt.Printf("spliting namespace and name, %s\n", err.Error())
+	}
 
+	deployment, err := c.deploymentsLister.Deployments(namespace).Get(name)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			klog.V(4).Infof("Deployment %s has been deleted", key)
+			return true
+		}
+		klog.Errorf("Error syncing deployment %s: %v", key, err)
+		c.workqueue.AddRateLimited(objRef)
+		return true
+	}
+	klog.Infof("Successfully processed deployment: %s", deployment.Name)
 	return true
 }
 func (c *Controller) handleAdd(obj interface{}) {
@@ -86,11 +99,18 @@ func (c *Controller) handleAdd(obj interface{}) {
 	c.workqueue.Add(obj)
 }
 
+func (c *Controller) handleUpdate(oldObj, newObj interface{}) {
+	newDepl := newObj.(*appsv1.Deployment)
+	oldDepl := oldObj.(*appsv1.Deployment)
+	if newDepl.ResourceVersion == oldDepl.ResourceVersion {
+		// Periodic resync will send update events for all known Deployments.
+		// Two different versions of the same Deployment will always have different RVs.
+		return
+	}
+	fmt.Printf("Update triggered for %s/%s\n", newDepl.Namespace, newDepl.Name)
+	c.workqueue.Add(newObj)
+}
 func (c *Controller) handleDel(obj interface{}) {
 	fmt.Println("hello del is called")
 	c.workqueue.Add(obj)
 }
-
-//func (c *Controller) handleUpdate(obj interface{}) {
-//
-//}
