29356e5638f82078df8337274ba85a12709601fa
Rishi Mondal <mavrickrishi@gmail.com>
2025-03-14 01:24:09 +0530

    No code changes made.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,18 +10,11 @@ import {
   Grid,
   Snackbar,
   Alert,
-  FormControlLabel,
-  Checkbox,
-  InputAdornment,
-  AlertColor,
+  AlertColor // Import AlertColor type
 } from "@mui/material";
 import { useNavigate, useLocation } from "react-router-dom";
 import LoadingFallback from "./LoadingFallback";
-import kubestellar_word from "../assets/kubes.png"
-import kubestellar_icon from "../assets/kubestellar-icon-color.png"
-import user from "../assets/user.png"
-import key from "../assets/key.png"
-
+import kube from "../assets/kubestellar.png";
 import "../index.css";
 
 const Profile = () => {
@@ -33,6 +26,7 @@ const Profile = () => {
   const [loading, setLoading] = useState(true);
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState("");
+  // Type snackbarSeverity with AlertColor
   const [snackbarSeverity, setSnackbarSeverity] = useState<AlertColor>("success");
   
   const navigate = useNavigate();
@@ -134,299 +128,204 @@ const Profile = () => {
       top: 0, 
       left: 0,
       zIndex: 1000,
-      background: "#99D8F7", // Solid bluish background for the entire page
     }}>
       <Container maxWidth={false} sx={{ height: "100%", padding: 0, margin: 0 }}>
-        <Grid container sx={{ height: "100%", width: "100vw", margin: 0 }}>
-          {/* left section */}
+        <Grid container sx={{ height: "100%" }}>
           <Grid 
             item 
             xs={12} 
-            md={6} 
+            md={9.4} 
             sx={{ 
               display: "flex", 
               flexDirection: "column", 
               justifyContent: "center", 
               alignItems: "center",
-              color: "#FFFFFF", // White text fill
+              color: "white",
               textAlign: "center",
               padding: 4,
+              background: "rgb(0, 81, 255)",
+              backgroundSize: "cover",
               height: "100vh",
               width: "100%",
-              position: "relative", // For positioning the diamond gradient overlay
             }}
           >
             <Typography 
-              variant="h1" 
+              variant="h2" 
               sx={{ 
-                mb: 14, 
+                mb: 12, 
                 fontFamily: "Got Milk Sans Serif, sans-serif",
-                WebkitTextStroke: "1px #5294F6", // Stroke color for "got multi-cluster!"
-                color: "#FFFFFF", // Fill color for text
               }}
             >
-              got multi-cluster?
+              got multi-cluster!
             </Typography>
-            {/* Diamond-shaped gradient overlay behind the icon */}
-            <Box
-              sx={{
-                position: "absolute",
-                top: "50%", // Center vertically
-                left: "50%", // Center horizontally
-                transform: "translate(-50%, -50%) rotate(45deg)", // Rotate to create a diamond shape
-                width: "600px", // Adjust size to match the image
-                height: "600px",
-                background: "radial-gradient(ellipse at center, #D4FCE4 20%, transparent 75%)", // Diamond gradient effect
-                zIndex: 1, // Behind the icon and text
-              }}
-            />
-            <img
-              src={kubestellar_icon}
-              alt="KubeStellar Small Logo"
-              style={{ 
-                width: "380px", 
-                opacity: 1, 
-                marginBottom: "10px", 
-                marginBlockEnd: "60px", 
-                position: "relative", 
-                zIndex: 2 // Above the gradient
-              }}
-            />
-            <img
-              src={kubestellar_word}
-              alt="KubeStellar Small Logo"
-              style={{ width: "650px", opacity: 1, position: "relative", zIndex: 2 }}
-            />
+            <Box sx={{ 
+              width: "200px", 
+              height: "200px", 
+              backgroundImage: `url('https://docs.kubestellar.io/release-0.26.0/logo.png')`,
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+            }} />
           </Grid>
-          {/* right section */}
           <Grid 
             item 
             xs={12} 
-            md={6} 
+            md={2.6} 
             sx={{ 
               display: "flex", 
               justifyContent: "center", 
-              alignItems: "center", 
+              alignItems: "center",
               background: "white",
               padding: 4,
               height: "100vh",
-              width: "100%", // Ensure full width
-              margin: 0, // Remove any margin
             }}
           >
             <Card sx={{ 
               width: "100%", 
-              maxWidth: 560, 
+              maxWidth: 360, 
               boxShadow: 0, 
               borderRadius: 0,
               background: "transparent",
-              display: "flex", 
+              height: "100%",
+              display: "flex",
               flexDirection: "column",
-              alignItems: "flex-start",
+              justifyContent: "center",
             }}>
               <CardContent sx={{ 
-                width: "100%", 
-                padding: "20px", 
                 display: "flex", 
-                flexDirection: "column",
-                alignItems: "flex-start",
+                flexDirection: "column", 
+                justifyContent: "center", 
+                flexGrow: 1, 
               }}>
-                <Typography 
-                  variant="h4" 
-                  sx={{ 
-                    mb: 6, 
-                    color: "#000000", 
-                    textAlign: "left", 
-                    fontWeight: "bold",
-                    whiteSpace: "nowrap",
-                    fontSize: "40px",
-                    overflow: "visible",
-                    fontFamily: "revert"
-                  }}
-                >
-                  Sign in to KubeStellar
-                </Typography>
-                <form onSubmit={handleSubmit} style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-                  {/* Username Label and Input with Icon */}
-                  <Box sx={{ width: "100%", mb: 3 }}>
-                    <Typography 
-                      variant="body1" 
-                      sx={{ 
-                        color: "gray", 
-                        textAlign: "left",
-                        fontSize: "25px",
-                        fontFamily: "system-ui"
-                      }}
-                    >
-                      Username
-                    </Typography>
-                    <TextField
-                      fullWidth
-                      variant="outlined"
-                      value={username}
-                      onChange={(e) => {
-                        setUsername(e.target.value);
-                        setUsernameError(false);
-                        setUsernameErrorMessage("");
-                      }}
-                      error={usernameError}
-                      helperText={usernameError ? usernameErrorMessage : ""}
-                      InputProps={{
-                        startAdornment: (
-                          <InputAdornment position="start">
-                            <img src={user} alt="User Icon" style={{ width: "40px", height: "35px" }} />
-                          </InputAdornment>
-                        ),
-                      }}
-                      sx={{ 
-                        "& .MuiOutlinedInput-root": {
-                          borderRadius: 2,
-                          height: "36px",
-                          "& .MuiInputBase-input": {
-                            padding: "8px 14px",
-                          },
-                          "& .MuiOutlinedInput-notchedOutline": {
-                            border: "3px solid gray", // Set 2px solid black border
-                          },
-                          "&:hover .MuiOutlinedInput-notchedOutline": {
-                            border: "3px solid gray", // Ensure border on hover
-                          },
-                          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                            border: "3px solid gray", // Ensure border on focus
-                          },
-                        },
-                        width: "100%",
-                      }}
-                    />
-                  </Box>
-                  {/* Password Label and Input with Icon */}
-                  <Box sx={{ width: "100%" }}>
-                    <Typography 
-                      variant="body1" 
-                      sx={{ 
-                        color: "gray", 
-                        textAlign: "left",
-                        fontSize: "25px",
-                        fontFamily: "system-ui"
-                      }}
-                    >
-                      Password
-                    </Typography>
+                <Box sx={{ 
+                  mt: -3,
+                  display: "flex", 
+                  justifyContent: "center", 
+                  mb: 6,
+                }}>
+                  <img
+                    src="https://docs.kubestellar.io/release-0.26.0/KubeStellar-with-Logo-transparent-v2.png" 
+                    alt="KubeStellar Logo"
+                    style={{ width: "250px" }}
+                  />
+                </Box>
+                <form onSubmit={handleSubmit} style={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
+                  <TextField
+                    fullWidth
+                    label="Username"
+                    variant="standard"
+                    value={username}
+                    onChange={(e) => {
+                      setUsername(e.target.value);
+                      setUsernameError(false);
+                      setUsernameErrorMessage("");
+                    }}
+                    error={usernameError}
+                    helperText={usernameError ? usernameErrorMessage : ""}
+                    margin="dense"
+                    sx={{ 
+                      "& .MuiInputBase-root": { 
+                        borderRadius: 0,
+                      },
+                      "& .MuiInput-underline:before": { 
+                        borderBottom: "1px solid gray",
+                      },
+                      "& .MuiInput-underline:after": { 
+                        borderBottom: "1px solid gray",
+                      },
+                      "& .MuiInput-underline:hover:not(.Mui-disabled):before": { 
+                        borderBottom: "1px solid gray",
+                      },
+                      minHeight: "70px",
+                    }}
+                  />
+                  <Box sx={{ mt: 3, position: "relative" }}>
                     <TextField
                       fullWidth
                       type="password"
-                      variant="outlined"
+                      label="Password"
+                      variant="standard"
                       value={password}
                       onChange={(e) => {
                         setPassword(e.target.value);
                         setPasswordError(false);
                       }}
                       error={passwordError}
-                      InputProps={{
-                        startAdornment: (
-                          <InputAdornment position="start">
-                            <img src={key} alt="Key Icon" style={{ width: "30px", height: "30px" }} />
-                          </InputAdornment>
-                        ),
-                      }}
+                      margin="dense"
                       sx={{ 
-                        "& .MuiOutlinedInput-root": {
-                          borderRadius: 2,
-                          height: "36px",
-                          "& .MuiInputBase-input": {
-                            padding: "8px 14px",
-                          },
-                          "& .MuiOutlinedInput-notchedOutline": {
-                            border: "3px solid gray", // Set 3px solid gray border
-                          },
-                          "&:hover .MuiOutlinedInput-notchedOutline": {
-                            border: "3px solid gray", // Ensure border on hover
-                          },
-                          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                            border: "3px solid gray", // Ensure border on focus
-                          },
+                        "& .MuiInputBase-root": { 
+                          borderRadius: 0,
                         },
-                        width: "100%",
+                        "& .MuiInput-underline:before": { 
+                          borderBottom: "1px solid gray",
+                        },
+                        "& .MuiInput-underline:after": { 
+                          borderBottom: "1px solid gray",
+                        },
+                        "& .MuiInput-underline:hover:not(.Mui-disabled):before": { 
+                          borderBottom: "1px solid gray",
+                        },
+                        minHeight: "70px",
                       }}
                     />
+                    {passwordError && (
+                      <Typography 
+                        sx={{ 
+                          color: "error.main", 
+                          position: "absolute",
+                          top: "100%", // Position below password field
+                          left: 0,
+                          right: 0,
+                          textAlign: "center",
+                          mt: 1, // Small margin to avoid overlap
+                        }}
+                      >
+                        Invalid username or password
+                      </Typography>
+                    )}
                   </Box>
-                  {/* Keep the "Keep me signed in" checkbox */}
-                  <Box sx={{ display: "flex", alignItems: "center", width: "100%", mt: 1, ml: "15px" }}>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          sx={{
-                            "&.MuiCheckbox-root": {
-                              borderRadius: "2px", // Apply border radius directly to the checkbox root
-                            },
-                            "& .MuiSvgIcon-root": {
-                              borderRadius: "2px", // Ensure the SVG icon (checkmark) also has the border radius
-                            },
-                          }}
-                        />
-                      }
-                      label="Keep me signed in"
-                      sx={{
-                        color: "gray",
-                        "& .MuiFormControlLabel-label": {
-                          fontSize: "20px", // Increase font size of the label
-                          fontWeight: "550",
-                          fontFamily: "system-ui"
-                        },
-                      }}
-                    />
-                  </Box>
-                  <Box sx={{ width: "100%" }}>
-                    <Button 
-                      type="submit" 
-                      variant="contained"
-                      fullWidth 
-                      sx={{ 
-                        mt: 2,
-                        backgroundColor: "#007AFF",
-                        color: "#FFFFFF",
-                        p: 0,
-                        borderRadius: 2,
-                        fontWeight: "bold",
-                        fontSize: "20px",
-                        textTransform: "none", // Prevent uppercase transformation
-                        "&:hover": {
-                          backgroundColor: "#0062CC",
-                        },
-                      }}
-                    >
-                      Sign In
-                    </Button>
-                  </Box>
-                  <Box sx={{ mt: 2, width: "100%", display: "flex", justifyContent: "center", gap: "7px" }}>
-                    <Typography 
-                      variant="body2" 
-                      sx={{ 
-                        mt: 0, 
-                        color: "gray", 
-                        fontWeight: "bold",
-                        textAlign: "center",
-                        fontSize: "16px"
-                      }}
-                    >
-                      Don’t have an account?
-                    </Typography>
-                    <Typography 
-                      variant="body2" 
-                      sx={{ 
-                        mt: 0, 
-                        color: "#007AFF", 
-                        textAlign: "center",
-                        textDecoration: "underline",
-                        cursor: "pointer",
-                        fontSize: "16px",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Register
-                    </Typography>
-                  </Box>
+                  <Button 
+                    type="submit" 
+                    variant="text"
+                    fullWidth 
+                    sx={{ 
+                      mt: 8, // Fixed margin-top to maintain position
+                      color: "rgb(5, 128, 243)",
+                      borderRadius: 2,
+                      fontWeight: "bold",
+                      p: "15px",
+                      border: "none",
+                      "&:hover": {
+                        backgroundColor: "rgba(0, 102, 255, 0.1)",
+                        color: "rgb(5, 128, 243)",
+                        border: "none",
+                      },
+                      "&:focus": {
+                        outline: "none",
+                        border: "none",
+                      },
+                      "&:active": {
+                        border: "none",
+                      },
+                    }}
+                  >
+                    SIGN IN
+                  </Button>
                 </form>
+                <Box sx={{ 
+                  display: "flex", 
+                  justifyContent: "center", 
+                  mb: -2,
+                  mt: "auto",
+                  textAlign: "center",
+                  alignItems: "center",
+                }}>
+                  <img
+                    src={kube}
+                    alt="KubeStellar Small Logo"
+                    style={{ width: "140px", opacity: 0.7 }}
+                  />
+                </Box>
               </CardContent>
             </Card>
           </Grid>
