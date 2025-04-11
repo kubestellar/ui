43bb17de3917edeecedfe5b782ed0bddaedc6ccd
Rishi Mondal <mavrickrishi@gmail.com>
2025-04-12 00:20:07 +0530

    feat: extend Window interface to include optional electron property
    Signed-off-by: Rishi Mondal <mavrickrishi@gmail.com>

--- src/service/api.ts
+++ src/service/api.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 // Extend the Window interface to include the electron property
 declare global {
   interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     electron?: any;
   }
 }
