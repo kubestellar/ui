88b5d61dd682c6705cfcf54e4b640f364f020a0f
Rahul Vishwakarma <rahulvs2809@gmail.com>
2025-04-17 18:46:21 +0530

    added some comment

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,11 +1,13 @@
 import PodTerminal from "./PodTerminal"
 const Terminal = () => {
 // ws://localhost:4000/ws/pod/test-fhn30/test-fhn30-66cf98cc85-fbjnv/shell/my-container?context=cluster2
+// Create your own and put the value here (just for testing I did)
+// In Progress
     return (
         <>
             <PodTerminal
                 namespace="test-fhn30"
-                pod="test-fhn30-66cf98cc85-fbjnv"
+                pod="test-fhn30-66cf98cc85-bqj45"
                 container="my-container"
                 context="cluster2"
             />
