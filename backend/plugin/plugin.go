89b6f4118437a254bef640435dd8adf2a5a1d344
ashish111333 <ashishkumar0265aaa@gmail.com>
2025-04-18 01:02:05 +0530

    comment fix
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- backend/plugin/plugin.go
+++ backend/plugin/plugin.go
@@ -6,7 +6,6 @@ import "github.com/gin-gonic/gin"
 // our backend using plugins
 
 // plugin interface defines methods that a KS plugin must implement
-// routes defines the routes to communicate with that
 
 type Plugin interface {
 	// name of the plugin
