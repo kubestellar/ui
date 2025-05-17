62bcb1a91e4280779100e5884fd3607e3c45e75d
ashish111333 <ashishkumar0265aaa@gmail.com>
2025-05-18 02:01:57 +0530

    use pvc instead of pv
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- backend/plugin/plugins/backup_plugin.go
+++ backend/plugin/plugins/backup_plugin.go
@@ -65,7 +65,13 @@ func rootHandler(c *gin.Context) {
 
 // takes snapshot of the cluster
 func takeSnapshot(c *gin.Context) {
-
+	freeBackupResources(bp.c)
+	err := createBackupJob(bp.c)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, nil)
 }
 func restoreFromSnapshot(c *gin.Context) {
 
@@ -81,23 +87,28 @@ func init() {
 		return
 	}
 	//
-	// currently only supporting postgres backend
+	// currently only supporting postgr for structuredes backend
 	bp = backupPlugin{
 		storageType: "postgres",
 		c:           c,
 	}
+
 	Pm.Register(bp)
 }
 
 // create job that takes backup
 func createBackupJob(c *kubernetes.Clientset) error {
-	//get db password
+
 	s, err := c.CoreV1().Secrets("kubeflex-system").Get(context.TODO(), "postgres-postgresql", metav1.GetOptions{})
 	if err != nil {
 		return err
 	}
 	var password []byte
 	_, err = base64.StdEncoding.Decode(s.Data["postgres-password"], password)
+	if err != nil {
+		return err
+	}
+	err = pvc(c)
 	if err != nil {
 		return err
 	}
@@ -127,11 +138,27 @@ func createBackupJob(c *kubernetes.Clientset) error {
 								},
 								corev1.EnvVar{
 									Name:  "host",
-									Value: "postgres-postgressql.kubeflex-system.svc.cluster.local",
+									Value: "postgres`-postgressql.kubeflex-system.svc.cluster.local",
 								},
 								corev1.EnvVar{
 									Name:  "user",
 									Value: "postgres",
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								corev1.VolumeMount{
+									Name:      "backup-vol",
+									MountPath: "/mnt/backup-vol",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						corev1.Volume{
+							Name: "backup-vol",
+							VolumeSource: corev1.VolumeSource{
+								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+									ClaimName: "backup-vol-claim",
 								},
 							},
 						},
@@ -148,25 +175,35 @@ func createBackupJob(c *kubernetes.Clientset) error {
 	return nil
 
 }
-func makePv(c *kubernetes.Clientset) error {
 
-	pv, err := c.CoreV1().PersistentVolumes().Create(context.TODO(), &corev1.PersistentVolume{
-
+func pvc(c *kubernetes.Clientset) error {
+	storageClass := "standard"
+	pvc, err := c.CoreV1().PersistentVolumeClaims("default").Create(context.TODO(), &corev1.PersistentVolumeClaim{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "pg-backup-vol",
+			Name:      "backup-vol-claim",
+			Namespace: "default",
 		},
-		Spec: corev1.PersistentVolumeSpec{
-			Capacity: corev1.ResourceList{
-				corev1.ResourceStorage: resource.MustParse("1Gi"),
+		Spec: corev1.PersistentVolumeClaimSpec{
+			Resources: corev1.VolumeResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceStorage: resource.MustParse("5Gi"),
+				},
 			},
 			AccessModes: []corev1.PersistentVolumeAccessMode{
 				corev1.ReadWriteOnce,
 			},
+			StorageClassName: &storageClass,
 		},
 	}, metav1.CreateOptions{})
 	if err != nil {
 		return err
 	}
-	log.LogInfo("created pv", zap.String("name", pv.Name))
+	log.LogInfo("created a pvc", zap.String("name", pvc.Name))
 	return nil
+
+}
+
+func freeBackupResources(c *kubernetes.Clientset) {
+	c.BatchV1().Jobs("default").Delete(context.TODO(), "pg-backup-job", *metav1.NewDeleteOptions(0))
+	c.CoreV1().PersistentVolumeClaims("default").Delete(context.TODO(), "backup-vol-claim", *metav1.NewDeleteOptions(0))
 }
