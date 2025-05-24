7dc0dc72d3d0595b4a1e073e1b63693db6e006db
Shivam Kumar <127589548+btwshivam@users.noreply.github.com>
2025-05-24 21:47:41 +0530

    Feat: Search , Filter Functionality and UI/UX improvement of cluster table and Quick Connect (#886)
    * Cluster Table, Quick Connect
    
    * Fix: Scroll-issue and Query dev tool
    
    * Fix: Context Breakdown
    
    * Fix: ContextName
    
    * FIX: Flickering and Dark mode consitancy
    
    * FIX: Flickering and Dark mode consitancy

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -1,13 +1,7 @@
 import { PropsWithChildren } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from './queryClient';
 
 export const QueryProvider = ({ children }: PropsWithChildren) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
