d49254a2aace485c557130d9b4e17d91be16a306
AayushSaini101 <kumaraayush9810@gmail.com>
2025-06-18 08:51:11 +0530

    Fix test case running

--- backend/test/auth/auth_test.go
+++ backend/test/auth/auth_test.go
@@ -1,19 +1,10 @@
 package auth_test
 
 import (
-	"context"
-	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/kubernetes/fake"
-	k8stesting "k8s.io/client-go/testing"
-
 	auth "github.com/kubestellar/ui/auth"
-	jwtconfig "github.com/kubestellar/ui/jwt"
 )
 
 func TestUserStorage(t *testing.T) {
@@ -42,3 +33,6 @@ func TestOverwriteUser(t *testing.T) {
 	assert.Equal(t, "newpass", user.Password)
 	assert.Equal(t, []string{"write"}, user.Permissions)
 }
+
+
+
