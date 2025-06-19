03c6ba51abe406b636a4027bb06e7420276d0002
Per0x1de-1337 <baviskarhd99@gmail.com>
2025-06-19 18:24:44 +0530

    minor fixes

--- src/components/QuickConnectTab.tsx
+++ src/components/QuickConnectTab.tsx
@@ -108,10 +108,7 @@ const QuickConnectTab: React.FC<QuickConnectProps> = ({
         setManualLoading(false); // Reset loading state
       }
     };
-  }, [showLogs, onboardingStatus, setOnboardingStatus, setOnboardingError]);
-  });
-
-  // This function will be called when the onboarding is completed via logs
+  }, [showLogs, onboardingStatus, setOnboardingStatus, setOnboardingError,setManualLoading]);
   const handleOnboardingComplete = () => {
     setTimeout(() => {
       setShowLogs(false);
