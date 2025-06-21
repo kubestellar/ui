39162eee754841e3b3d8772cb30b298c1c5f1e31
Gaurab Khanal <khanalgaurab98@gmail.com>
2025-06-21 17:15:31 +0545

    feat: add make migrate command

--- backend/postgresql/config.go
+++ backend/postgresql/config.go
@@ -23,10 +23,10 @@ func ConnectDB() {
 	LoadConfig()
 
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"),
 		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_DB"), os.Getenv("POSTGRES_SSL_MODE"),
 	)
 
 	var err error
