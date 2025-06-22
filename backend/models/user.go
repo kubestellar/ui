7b50dd19f8a3b275254059da58c753ec35b2a753
Gaurab Khanal <khanalgaurab98@gmail.com>
2025-06-22 15:17:02 +0545

    chore: add default permission to admin in user struct

--- backend/models/user.go
+++ backend/models/user.go
@@ -19,24 +19,26 @@ type User struct {
 	ID          uint           `gorm:"primaryKey;autoIncrement"`
 	Username    string         `gorm:"unique;not null"`
 	Password    string         `gorm:"not null"`
-	Permissions pq.StringArray `gorm:"type:user_permission_enum[];not null;default:'{read}'"`
+	Permissions pq.StringArray `gorm:"type:user_permission_enum[];not null;default:'{admin}'"`
 	CreatedAt   time.Time      `gorm:"autoCreateTime"`
 	UpdatedAt   time.Time      `gorm:"autoUpdateTime"`
 }
 
 type UserResponse struct {
-	ID        uint      `json:"id"`
-	Username  string    `json:"username"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID          uint      `json:"id"`
+	Username    string    `json:"username"`
+	Permissions []string  `json:"permissions"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 func (u *User) ToResponse() UserResponse {
 	return UserResponse{
-		ID:        u.ID,
-		Username:  u.Username,
-		CreatedAt: u.CreatedAt,
-		UpdatedAt: u.UpdatedAt,
+		ID:          u.ID,
+		Username:    u.Username,
+		Permissions: u.Permissions,
+		CreatedAt:   u.CreatedAt,
+		UpdatedAt:   u.UpdatedAt,
 	}
 }
 
