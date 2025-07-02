578351736e247867b177e0342b97c9256c6cd167
arpit529srivastava <arpitsrivastava529@gmail.com>
2025-07-02 14:41:49 +0530

    ✨feat: add unit tests for Cluster, Namespace, and User models
    Signed-off-by: arpit529srivastava <arpitsrivastava529@gmail.com>

---
+++ backend/test/models/user_test.go
@@ -0,0 +1,18 @@
+package models_test
+
+import (
+	"testing"
+
+	"github.com/kubestellar/ui/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHashAndCheckPassword(t *testing.T) {
+	password := "mysecret"
+	hash, err := models.HashPassword(password)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, hash)
+
+	assert.True(t, models.CheckPasswordHash(password, hash))
+	assert.False(t, models.CheckPasswordHash("wrongpassword", hash))
+} 
