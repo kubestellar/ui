b012d8d7605936966661ab417735e1c499f6b1b8
alokdangre <alokdangre@gmail.com>
2025-09-20 02:22:45 +0530

    feat(tests): enhance installation test flow by adding missing steps and retry button interaction

--- frontend/e2e/Install.spec.ts
+++ frontend/e2e/Install.spec.ts
@@ -2,9 +2,12 @@ import { test } from '@playwright/test';
 
 test('install', async ({ page }) => {
   await page.goto('http://localhost:5173/install');
+  await page.getByRole('button', { name: 'Retry Connection' }).click();
   await page.getByRole('heading', { name: 'Welcome to KubeStellar' }).click();
   await page.getByText('Get started with KubeStellar').click();
   await page.getByRole('button', { name: 'Next: Installation' }).click();
-  await page.getByRole('button', { name: 'kind' }).click();
   await page.getByRole('button', { name: 'k3d' }).click();
+  await page.getByRole('button', { name: 'Start Installation' }).click();
+  await page.getByText('Follow the CLI installation').click();
+  await page.getByText('Follow the CLI installation').click();
 });
