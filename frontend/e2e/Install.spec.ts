e9927d773e73d7aa5730e69dc255679611e387a6
alokdangre <alokdangre@gmail.com>
2025-09-20 11:29:46 +0530

    fix(tests): remove redundant 'Retry Connection' button click in installation test

--- frontend/e2e/Install.spec.ts
+++ frontend/e2e/Install.spec.ts
@@ -2,7 +2,6 @@ import { test } from '@playwright/test';
 
 test('install', async ({ page }) => {
   await page.goto('http://localhost:5173/install');
-  await page.getByRole('button', { name: 'Retry Connection' }).click();
   await page.getByRole('heading', { name: 'Welcome to KubeStellar' }).click();
   await page.getByText('Get started with KubeStellar').click();
   await page.getByRole('button', { name: 'Next: Installation' }).click();
